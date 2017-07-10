fcd8d3023a4f05c256b8ab7a0023f233fa124d96
Derek Neuts <dcneuts@gmail.com>
2017-07-09 18:44:17 -0700

    Additional code for API, should verify to status 200

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 /**
  * Created by derek on 7/9/17.
  */
+"use strict";
+const https = require('https');
+
+//Establishing global constant for username to be pulled via API
+const username = "dcneuts";
+
 //Print message to console
 function printMessage(username, badgeCount, points) {
-	const MESSAGE = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-	console.log(MESSAGE);
+	var message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+	console.log(message);
 }
+
+//Connect to the Treehouse API
+const request = https.get(`https://teamtreehouse.com/dcneuts.json`, response => {
+	console.dir(response.statusCode);//Status of 200 should return as successful connect
+});
