5d217394131fe8fee98d871a0e72707f60bb7f7f
Derek Neuts <dcneuts@gmail.com>
2017-07-09 20:38:54 -0700

    Wrapped API connection for data in function getProfile and then called it below the function block

--- app.js
+++ app.js
@@ -2,27 +2,30 @@
  * Created by derek on 7/9/17.
  */
 "use strict";
-const https = require('https');
-
-//Establishing global constant for username to be pulled via API
-const username = "dcneuts";
+const https = require ('https');
 
 //Print message to console
-function printMessage(username, badgeCount, points) {
+function printMessage (username, badgeCount, points) {
 	const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-	console.log(message);
+	console.log (message);
 }
 
+
+function getProfile (username) {
 //Connect to the Treehouse API
-const request = https.get(`https://teamtreehouse.com/dcneuts.json`, response => {
-	let body = "";
-	
-	response.on('data', data => {
-		body += data.toString();
+	const request = https.get (`https://teamtreehouse.com/dcneuts.json`, response => {
+		let body = "";
+		
+		response.on ('data', data => {
+			body += data.toString ();
+		});
+		
+		response.on ('end', () => {
+			const profile = JSON.parse (body);
+			printMessage (username, profile.badges.length, profile.points.JavaScript);
+		});
 	});
 	
-	response.on('end', () => {
-		const profile = JSON.parse(body);
-		printMessage(username, profile.badges.length, profile.points.JavaScript);
-	});
-});
+}
+
+getProfile("dcneuts");
