fb7711e801016cd10c1f0cd1b78eaf6c5ff26539
Derek Neuts <dcneuts@gmail.com>
2017-07-09 23:01:19 -0700

    Added http.STATUS_CODES method to the response to get friendlier errors in the console, added http method to beginning of script to interpret codes

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
  * Created by derek on 7/9/17.
  */
 "use strict";
+//For connection
 const https = require ('https');
+//For status codes
+const http = require ('http');
 
 //General error handling
-function printError(error) {
-	console.error(error.message);
+function printError (error) {
+	console.error (error.message);
 }
 
 //Print message to console
@@ -20,25 +23,33 @@ function getProfile (username) {
 	try {
 //Connect to the Treehouse API
 		const request = https.get (`https://teamtreehouse.com/${username}.json`, response => {
-			let body = "";
-			
-			response.on ('data', data => {
-				body += data.toString ();
-			});
-			
-			response.on ('end', () => {
-				try {
-					const profile = JSON.parse (body);
-					printMessage (username, profile.badges.length, profile.points.JavaScript);
-				} catch (error) {
-					printError(error);
-				}
-			});
+			//Wrapping the blocks with if statement for status code based error messages
+			if (response.statusCode == 200) {
+				let body = "";
+				
+				response.on ('data', data => {
+					body += data.toString ();
+				});
+				
+				response.on ('end', () => {
+					try {
+						const profile = JSON.parse (body);
+						printMessage (username, profile.badges.length, profile.points.JavaScript);
+					} catch (error) {
+						printError (error);
+					}
+				});
+				// using the else statement to print friendlier error message based on 404
+			} else {
+				const message         = `There was an error getting the profile for ${username} (${http.STATUS_CODES[response.statusCode]})`;
+				const statusCodeError = new Error (message);
+				printError (statusCodeError);
+			}
 		});
 		request.on ('error', error => console.error (`Problem with request: ${error.message}`));
 		// use 'catch' at the end of the nested 'try' code block to capture error and then console log via error method
 	} catch (error) {
-		printError(error);
+		printError (error);
 	}
 }
 //const users = ["dcneuts", "chalkers", "davemcfarland"];
