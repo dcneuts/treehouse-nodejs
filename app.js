5af91cbaa3e747cc9fade6bb11a7192a96a78ff8
Derek Neuts <dcneuts@gmail.com>
2017-07-09 19:08:29 -0700

    Parsed the JSON data into readable text and drilling down which data would be most useful to output

--- app.js
+++ app.js
@@ -9,11 +9,20 @@ const username = "dcneuts";
 
 //Print message to console
 function printMessage(username, badgeCount, points) {
-	var message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+	const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
 	console.log(message);
 }
 
 //Connect to the Treehouse API
 const request = https.get(`https://teamtreehouse.com/dcneuts.json`, response => {
-	console.dir(response.statusCode);//Status of 200 should return as successful connect
+	let body = "";
+	
+	response.on('data', data => {
+		body += data.toString();
+	});
+	
+	response.on('end', () => {
+		const profile = JSON.parse(body);
+		printMessage(username, profile.badges.length, profile.points.JavaScript);
+	});
 });
