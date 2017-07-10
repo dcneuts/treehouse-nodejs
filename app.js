57b16a12a46bcf92f43fe65093900fe707101589
Derek Neuts <dcneuts@gmail.com>
2017-07-09 18:33:13 -0700

    Setting up app.js for API test and added profile.js per the instructions

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 /**
  * Created by derek on 7/9/17.
  */
+//Print message to console
+function printMessage(username, badgeCount, points) {
+	const MESSAGE = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+	console.log(MESSAGE);
+}
