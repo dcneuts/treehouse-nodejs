558cce61bb702467b96a45eb2e5813b06b0c1c81
Derek Neuts <dcneuts@gmail.com>
2017-07-09 21:16:45 -0700

    Used try with catch to capture error during testing, confirmed working by removing protocol from get method

--- app.js
+++ app.js
@@ -10,24 +10,28 @@ function printMessage (username, badgeCount, points) {
 	console.log (message);
 }
 
-
+//Added 'try' to attempt to catch additional errors
 function getProfile (username) {
+	try {
 //Connect to the Treehouse API
-	const request = https.get (`https://teamtreehouse.com/${username}.json`, response => {
-		let body = "";
-		
-		response.on ('data', data => {
-			body += data.toString ();
+		const request = https.get (`https://teamtreehouse.com/${username}.json`, response => {
+			let body = "";
+			
+			response.on ('data', data => {
+				body += data.toString ();
+			});
+			
+			response.on ('end', () => {
+				const profile = JSON.parse (body);
+				printMessage (username, profile.badges.length, profile.points.JavaScript);
+			});
 		});
-		
-		response.on ('end', () => {
-			const profile = JSON.parse (body);
-			printMessage (username, profile.badges.length, profile.points.JavaScript);
-		});
-	});
-	request.on('error', error => console.error(`Problem with request: ${error.message}`));
+		request.on ('error', error => console.error (`Problem with request: ${error.message}`));
+		// use 'catch' at the end of the nested 'try' code block to capture error and then console log via error method
+	} catch (error) {
+		console.error(error.message);
+	}
 }
-
 //const users = ["dcneuts", "chalkers", "davemcfarland"];
-const users = process.argv.slice(2);
-users.forEach(getProfile);
+const users = process.argv.slice (2);
+users.forEach (getProfile);
